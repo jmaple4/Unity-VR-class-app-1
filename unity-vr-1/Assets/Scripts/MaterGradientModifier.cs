04aba98fdaa86aebf31a46d06ffbe3b0144091aa
Nick Kovacevich <jmaple4@gmail.com>
2018-06-04 23:46:42 -0700

    First check in for my VR unity app project

---
+++ unity-vr-1/Assets/Scripts/MaterGradientModifier.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Renderer))]
+
+public class MaterGradientModifier : MonoBehaviour
+{
+	Renderer _renderer;
+
+	// public Color myColor;
+
+	[SerializeField] Gradient gradient;
+
+	float _gradientPosition;
+	public float gradientPosition 
+	{
+		get { return _gradientPosition ; }
+		set
+		{
+			if (_gradientPosition != value)
+			{
+				_gradientPosition = value;
+				_renderer.material.color = gradient.Evaluate (_gradientPosition);
+			}
+		}
+	}
+	// void SetGradientPosition (float position) {
+	// 	if (position == gradientPosition)
+	// 	return;
+
+	// 	gradientPosition = position;
+	// 	_renderer.material.color = gradient.Evaluate (gradientPosition);
+	// }
+
+	void Awake ()
+	{
+		_renderer = GetComponent<Renderer>();
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		gradientPosition = 0;
+		// SetGradientPosition(0);
+	}
+	
+	// Update is called once per frame
+	// void Update()
+	// {
+	// 	// _renderer.material.color = gradient.Evaluate(gradientPosition);
+	// 	SetGradientPosition(Mathf.Sin (((Time.time)) * 0.5f) + 0.5f );
+	// }
+}
---
+++ unity-vr-1/Assets/Scripts/ObjectFocus.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFocus : MonoBehaviour
+{
+
+	[SerializeField] Transform reference;
+
+	[SerializeField] float minAngle = 10;
+	[SerializeField] float maxAngle = 30;
+
+	private float _fadeAmount;
+	public float fadeAmount
+	{
+		get { return _fadeAmount; }
+		set
+		{
+			if (value != _fadeAmount)
+			{
+				_fadeAmount = value;
+			}
+		}
+	}
+	private float _delta;
+	public float delta
+	{
+		get { return _delta; }
+		set
+		{
+			if (value != _delta)
+			{
+				_delta = value;
+				// TODO: update fade amount
+			}
+		}
+	}
+	void Fade () {
+		
+		// Debug.DrawLine (reference.position, transform.position, Color.yellow);
+
+		Vector3 d = (transform.position - reference.position).normalized;
+		
+		Debug.DrawRay (reference.position, d, Color.yellow);
+		Debug.DrawRay (reference.position, reference.forward, Color.cyan);
+		
+		delta = Vector3.Angle (d.normalized, reference.forward);
+	}
+	void Awake ()
+	{
+		// reference = GameObject.FindGameObjectWithTag ("MainCamera").transform;
+		if (!reference)
+			reference = Camera.main.transform;
+	}
+
+	void Update ()
+	{
+		Fade ();
+	}
+
+	
+	void OnGUI()
+	{
+		GUILayout.Label ("delta : " + delta.ToString());
+		GUILayout.Label ("fadeAmount : " + fadeAmount.ToString());
+	}
+}
