74fd5133287fbde65e6093e33abe14fe2b10a086
Nick Kovacevich <jmaple4@gmail.com>
2018-06-11 23:47:50 -0700

    Obj manager C# file

---
+++ unity-vr-1/Assets/Scripts/ObjectFocusManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFocusManager : MonoBehaviour 
+{
+
+	List<ObjectFocusManager> objectsInRange = new List<ObjectFocusManager>();
+
+	#region MonoBehaviour
+	void Start () 
+	{
+
+	}
+
+	void Update () 
+	{
+
+	}
+	#endregion
+}
