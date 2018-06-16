e7bf0d4c94ebbe61a35e56eda55c678ec33b465a
Nick Kovacevich <jmaple4@gmail.com>
2018-06-16 15:49:50 -0700

    Progress bar on touch

--- unity-vr-1/Assets/Scripts/TouchInputManager.cs
+++ unity-vr-1/Assets/Scripts/TouchInputManager.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using System;
 
-public class TouchInputManager : MonoBehaviour {
+public class TouchInputManager : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	[Serializable]
+	public class FloatEvent : UnityEvent<float> {}
+
+	[SerializeField] float delay = 2;
+
+	[SerializeField] UnityEvent onTouch;
+	[SerializeField] UnityEvent onTouchCancel;
+	[SerializeField] AnimationCurve timerUpdateCurve;
+	[SerializeField] FloatEvent onTouchTimerUpdate;
+	[SerializeField] UnityEvent onTouchTimerEnd;
+
+	float timer;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		if (Input.touchCount > 0)
+		{
+			switch (Input.touches[0].phase)
+			{
+				case TouchPhase.Began:
+					timer = 0;
+					onTouch.Invoke();
+					break;
+				case TouchPhase.Ended:
+					onTouchCancel.Invoke();
+					break;
+				default:
+					timer += Time.deltaTime;
+					onTouchTimerUpdate.Invoke (timerUpdateCurve.Evaluate (Mathf.InverseLerp(0, delay, timer)));
+					if (timer > delay)
+						onTouchTimerEnd.Invoke();
+					break;
+			}
+		}
 	}
 }
