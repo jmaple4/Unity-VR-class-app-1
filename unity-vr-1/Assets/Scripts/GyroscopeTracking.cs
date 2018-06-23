670a3ea00854ea4b994c9d5263c6259b06e6c475
Nick Kovac <jmaple4@gmail.com>
2018-06-22 23:49:50 -0700

    Gyroscopic roation
    Probably wrote some bad code here.

---
+++ unity-vr-1/Assets/Scripts/GyroscopeTracking.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroscopeTracking : MonoBehaviour {
+
+	#if UNITY_EDITOR
+
+	void Awake ()
+	{
+		if (!enabled)
+			return;
+		transform.parent = new GameObject("Gyro Root").transform;
+		transform.parent.rotation = Quaternion.Euler (0, 0, 60);
+		transform.rotation = Quaternion.identity;
+	}
+
+	void Start () 
+	{
+		Input.gyro.enabled = true;
+	}
+	
+	void Update () 
+	{
+		transform.localRotation = GyroToUnity (Input.gyro.attitude);
+	}
+
+	private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+	#endif
+}
