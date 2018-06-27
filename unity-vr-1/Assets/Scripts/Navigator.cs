1e1bb8161de81d81224143d1c49627e4ea5a87cd
Nick Kovacevich <jmaple4@gmail.com>
2018-06-26 22:26:41 -0700

    starting ray casting
    I've got a long yellow ray and nothing to point it at

---
+++ unity-vr-1/Assets/Scripts/Navigator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Navigator : MonoBehaviour 
+{
+	[Header ("Ray Casting")]
+	[SerializeField] float wallSearchDistance = 5;
+
+	[Header ("Debug")]
+	[SerializeField] bool drawLines = true;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		#if UNITY_EDITOR
+		if (drawLines)
+		{
+			Debug.DrawRay(transform.position, transform.forward * wallSearchDistance, Color.yellow);
+		}
+		#endif
+	}
+}
