60874111c643117f9c689f67f2696913274be1d1
Nick Kovacevich <jmaple4@gmail.com>
2018-06-27 22:54:43 -0700

    vertical ray after horizontal collider bounce

--- unity-vr-1/Assets/Scripts/Navigator.cs
+++ unity-vr-1/Assets/Scripts/Navigator.cs
@@ -8,6 +8,7 @@ public class Navigator : MonoBehaviour
 	[Header ("Ray Casting")]
 	[SerializeField] float wallSearchDistance = 5;
 	[SerializeField] float offWallDistance = .5f;
+	[SerializeField] float groundSearchDistance = 5;
 
 
 	[SerializeField] LayerMask cullingMask = -5;
@@ -19,6 +20,24 @@ public class Navigator : MonoBehaviour
 	Vector3 _wallHitNormal;
 	Vector3 _wallBouncePosition;
 
+	bool _groundHit;
+	RaycastHit _groundHitInfo;
+	Vector3 _groundHitPosition;
+
+	Vector3 _targetLocation;
+
+	private bool _hasTargetLocation;
+	public bool hasTargetLocation
+	{
+		get { return _hasTargetLocation; }
+		set
+		{
+			if (value != _hasTargetLocation)
+			{
+				_hasTargetLocation = value;
+			}
+		}
+	}
 
 	[Header ("Debug")]
 	[SerializeField] bool drawLines = true;
@@ -31,7 +50,7 @@ public class Navigator : MonoBehaviour
 	void Update () 
 	{
 
-		_wallHit = Physics.Raycast(transform.position, transform.forward, out _wallHitInfo, wallSearchDistance + offWallDistance, cullingMask);
+		_wallHit = Physics.Raycast(transform.position, transform.forward, out _wallHitInfo, wallSearchDistance + offWallDistance, cullingMask, queryTriggerInteraction);
 		if (_wallHit)
 		{
 			_wallHitPosition = _wallHitInfo.point;
@@ -45,11 +64,23 @@ public class Navigator : MonoBehaviour
 			_wallBouncePosition = _wallHitPosition;
 		}
 
+		_groundHit = Physics.Raycast(_wallBouncePosition, Vector3.down, out _groundHitInfo, groundSearchDistance, cullingMask, queryTriggerInteraction);
+		if (_groundHit)
+		{
+			_groundHitPosition = _groundHitInfo.point;
+		}
+		else 
+		{
+			hasTargetLocation = false;
+		}
+
 		#if UNITY_EDITOR
 		if (drawLines)
 		{
 			Debug.DrawLine(transform.position, _wallHitPosition, _wallHit ? Color.green : Color.yellow);
 			Debug.DrawLine(_wallHitPosition, _wallBouncePosition, Color.blue);
+			if (_groundHit)
+				Debug.DrawLine(_wallBouncePosition, _groundHitPosition, Color.cyan);
 			// Debug.DrawRay(transform.position, transform.forward * wallSearchDistance, Color.yellow);
 		}
 		#endif
