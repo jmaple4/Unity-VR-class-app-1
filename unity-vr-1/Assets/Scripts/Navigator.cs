acbb073740565f12751e30a096fd24ddb2038aea
Nick Kovacevich <jmaple4@gmail.com>
2018-06-27 21:04:24 -0700

    ray casting: horizontal bounce

--- unity-vr-1/Assets/Scripts/Navigator.cs
+++ unity-vr-1/Assets/Scripts/Navigator.cs
@@ -7,10 +7,18 @@ public class Navigator : MonoBehaviour
 {
 	[Header ("Ray Casting")]
 	[SerializeField] float wallSearchDistance = 5;
+	[SerializeField] float offWallDistance = .5f;
+
+
+	[SerializeField] LayerMask cullingMask = -5;
+	[SerializeField] QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.Ignore;
 
 	bool _wallHit;
 	RaycastHit _wallHitInfo;
 	Vector3 _wallHitPosition;
+	Vector3 _wallHitNormal;
+	Vector3 _wallBouncePosition;
+
 
 	[Header ("Debug")]
 	[SerializeField] bool drawLines = true;
@@ -23,20 +31,25 @@ public class Navigator : MonoBehaviour
 	void Update () 
 	{
 
-		_wallHit = Physics.Raycast(transform.position, transform.forward, out _wallHitInfo, wallSearchDistance);
+		_wallHit = Physics.Raycast(transform.position, transform.forward, out _wallHitInfo, wallSearchDistance + offWallDistance, cullingMask);
 		if (_wallHit)
 		{
 			_wallHitPosition = _wallHitInfo.point;
+			_wallHitNormal = _wallHitInfo.normal;
+			_wallBouncePosition = _wallHitPosition + _wallHitNormal * offWallDistance;
 		}
 		else 
 		{
 			_wallHitPosition = transform.position + transform.forward * wallSearchDistance;
+			// _wallHitNormal = -transform.forward;
+			_wallBouncePosition = _wallHitPosition;
 		}
 
 		#if UNITY_EDITOR
 		if (drawLines)
 		{
 			Debug.DrawLine(transform.position, _wallHitPosition, _wallHit ? Color.green : Color.yellow);
+			Debug.DrawLine(_wallHitPosition, _wallBouncePosition, Color.blue);
 			// Debug.DrawRay(transform.position, transform.forward * wallSearchDistance, Color.yellow);
 		}
 		#endif
