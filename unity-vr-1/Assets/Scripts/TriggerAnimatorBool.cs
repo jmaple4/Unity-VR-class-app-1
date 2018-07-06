59667f0f620d0f42a617a0adfa5fb0b480a04fcb
Nick Kovacevich <jmaple4@gmail.com>
2018-07-05 22:48:04 -0700

    applying door trigger logic to other doors
    There are no more video in the course, just articles to follow.

--- unity-vr-1/Assets/Scripts/TriggerAnimatorBool.cs
+++ unity-vr-1/Assets/Scripts/TriggerAnimatorBool.cs
@@ -8,17 +8,30 @@ public class TriggerAnimatorBool : MonoBehaviour
 	[SerializeField] string filterTag = "Player";
 	[SerializeField] Animator animator;
 	[SerializeField] string animatorBoolName;
+	[SerializeField] bool trueOnExit;
+	int _animatorBoolID;
 
-	
+	void Awake ()
+	{
+		if (!animator)
+			animator = GetComponentInParent<Animator>();
+		_animatorBoolID = Animator.StringToHash(animatorBoolName);
+	}
+
 	void OnTriggerEnter (Collider other)
 	{
+		if (!other.CompareTag(filterTag))
+			return;
+
 		// Debug.Log(other.name + "has entered " + gameObject.name, gameObject);
-		animator.SetBool(animatorBoolName, true);
+		animator.SetBool(_animatorBoolID, !trueOnExit);
 	}
 	void OnTriggerExit (Collider other) 
 	{
+		if (!other.CompareTag(filterTag))
+			return;
 		// Debug.Log(other.name + "has exited " + gameObject.name, gameObject);
-		animator.SetBool(animatorBoolName, false);
+		animator.SetBool(_animatorBoolID, trueOnExit);
 
 	}
 }
