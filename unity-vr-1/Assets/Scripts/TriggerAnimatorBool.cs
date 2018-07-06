df1ba529430c63a9320d1520324a14e37794d831
Nick Kovacevich <jmaple4@gmail.com>
2018-07-05 20:11:45 -0700

    Door trigger

---
+++ unity-vr-1/Assets/Scripts/TriggerAnimatorBool.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Collider))]
+public class TriggerAnimatorBool : MonoBehaviour 
+{
+	[SerializeField] string filterTag = "Player";
+	[SerializeField] Animator animator;
+	[SerializeField] string animatorBoolName;
+
+	
+	void OnTriggerEnter (Collider other)
+	{
+		// Debug.Log(other.name + "has entered " + gameObject.name, gameObject);
+		animator.SetBool(animatorBoolName, true);
+	}
+	void OnTriggerExit (Collider other) 
+	{
+		// Debug.Log(other.name + "has exited " + gameObject.name, gameObject);
+		animator.SetBool(animatorBoolName, false);
+
+	}
+}
